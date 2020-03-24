048eea4c6f3e0280287e8d65b8dfb3d2874f753c
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-03-23 22:57:46 -0300

    PlaceData, PlaceDAO

---
+++ lib/dao/place_dao.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hawks_shop/datas/place_data.dart';
+
+class PlaceDAO{
+
+  final String _placesCollection = "places";
+
+  Future<List<PlaceData>> getPlaces() async{
+    QuerySnapshot placesDoc = await Firestore.instance.collection(_placesCollection).getDocuments();
+    List<PlaceData> places = placesDoc.documents.map(_firebasePlaceToPlaceData).toList();
+    return places;
+  }
+
+  PlaceData _firebasePlaceToPlaceData(DocumentSnapshot firebasePlace){
+    return PlaceData.fromFirebaseDocument(firebasePlace);
+  }
+}
--- lib/tabs/places_tab.dart
+++ lib/tabs/places_tab.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hawks_shop/dao/place_dao.dart';
+import 'package:hawks_shop/datas/place_data.dart';
 import 'package:hawks_shop/tiles/place_tile.dart';
 
 class PlacesTab extends StatelessWidget {
@@ -7,8 +9,9 @@ class PlacesTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-      future: Firestore.instance.collection("places").getDocuments(),
+    PlaceDAO placeDAO = PlaceDAO();
+    return FutureBuilder<List<PlaceData>>(
+      future: placeDAO.getPlaces(),
       builder: (context, snapshot){
         if(!snapshot.hasData){
           return Center(
@@ -16,7 +19,7 @@ class PlacesTab extends StatelessWidget {
           );
         }
         return ListView(
-          children: snapshot.data.documents.map((doc) => PlaceTile(doc)).toList(),
+          children: snapshot.data.map((doc) => PlaceTile(doc)).toList(),
         );
       },
     );

9ba65708b49bcbd48d5c04b588af8d2b85870f09
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-03-23 22:43:12 -0300

    HomeDAO, HomeData, Função Map Order

---
+++ lib/dao/home_dao.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hawks_shop/datas/home_data.dart';
+
+class HomeDAO{
+
+  final String _homeCollecion = "home";
+
+  Future<List<HomeData>> getHomeData() async{
+    QuerySnapshot homeDataDocList = await Firestore.instance.collection(_homeCollecion)
+                                                            .orderBy("pos")
+                                                            .getDocuments();
+    List<HomeData> homeDataList = homeDataDocList.documents.map(_firebaseHomeToHomeData).toList();
+    return homeDataList;
+  }
+
+  HomeData _firebaseHomeToHomeData(DocumentSnapshot firebaseHome){
+    return HomeData.fromFirebaseDocument(firebaseHome);
+  }
+}
